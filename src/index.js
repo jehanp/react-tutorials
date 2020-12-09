19a27971db8d5c5422a149251ffe9a7815b6cb20
jehanp <jehanp@zone24x7.com>
2020-12-09 17:47:52 +0530

    Add handlClick function for square onClick

--- src/index.js
+++ src/index.js
@@ -12,7 +12,9 @@ import './index.css';
 
     render() {
       return (
-        <button className="square" onClick={() => {alert('click'); }}>
+        <button 
+        className="square" 
+        onClick={() => this.props.onClick()}>
           {this.props.value}
         </button>
       );
@@ -20,8 +22,26 @@ import './index.css';
   }
   
   class Board extends React.Component {
+    constructor(props){
+      super(props);
+      this.state = {
+        squares: Array(9).fill(null),
+      }
+    }
+
+    handleClick(i){
+      const squares = this.state.squares.slice();
+      squares[i] = 'X';
+      this.setState({squares: squares})
+    }
+
     renderSquare(i) {
-      return <Square value={i}/>;
+      return (
+        <Square 
+          value={this.state.squares[i]}
+          onClick = {() => this.handleClick(i)}
+        />
+      );
     }
   
     render() {
